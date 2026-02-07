17770103dc48b69dee328ddf8444714543db99e3
Agustín <agustinluporini@gmail.com>
2026-02-07 13:25:07 -0300

    prueba 4

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Dejá esto vacío. No fuerces rutas si estás en tu propio dominio.
-  
+  basePath: "/menu",
+
   images: {
     remotePatterns: [
       {

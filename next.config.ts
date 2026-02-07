0d3fcec8f4869e1c9630f0b8a5cb703df853303c
Agustín <agustinluporini@gmail.com>
2026-02-07 13:19:04 -0300

    Prueba deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Dejalo vacío por ahora. Vercel se encarga de todo.
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Esto permite cargar imágenes de Supabase/cualquier lado
+      },
+    ],
+  },
 };
 
 export default nextConfig;
